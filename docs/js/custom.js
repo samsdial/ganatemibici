d46bb625a83048648a18ea93b4e306806e967615
samsdial <diastudios@gmail.com>
2018-07-12 11:21:11 -0500

    Add Class learnig Javascript -- Sobre Carga de Operadores

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";$(function(){$('[data-toggle="tooltip"]').tooltip(),$(".slider").slick({autoplay:!0})});
+"use strict";function crearProducto(r,o){r=r||"Sin Nombre",o=o||0,console.log("Producto: ",r,"Precio: ",o)}function crearProducto100(r){crearProducto(r,100)}function crearProductoCamisa(r){crearProducto("camisa",r)}crearProducto("Lapiz"),crearProducto100("corrector"),crearProductoCamisa(75);
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,16 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('.slider').slick({
-        autoplay: true
-    });
-});
+// Sobre Carga de operadores
+//
+function crearProducto(nombre, precio) {
+    nombre = nombre || "Sin Nombre";
+    precio = precio || 0;
+    console.log("Producto: ", nombre, "Precio: ", precio);
+}
+function crearProducto100(nombre) {
+    crearProducto(nombre, 100);
+}
+function crearProductoCamisa( precio ) {
+    crearProducto( "camisa", precio)
+}
+crearProducto("Lapiz");
+crearProducto100("corrector");
+crearProductoCamisa(75);
