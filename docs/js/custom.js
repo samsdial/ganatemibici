4ea3fed4a94474d24dba0258536ef4a6e0d2191e
samsdial <diastudios@gmail.com>
2018-07-13 10:49:35 -0500

    Add Class learnig Javascript -- Objeto String

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";var a=new Boolean;console.log(a.valueOf()),a.valueOf()&&console.log("me imprimi!!");
+"use strict";var b="Herrera",a=new String("Fernando Herrera Pineda Chacon");console.log(a[0]),console.log(a.toUpperCase()),console.log(a.toLowerCase());var i=a.indexOf("Herrera");console.log("la letra esta: ",i);var i=a.lastIndexOf("n");console.log("la letra esta: ",i);var nombre=a.substr(0,a.indexOf(" "));console.log(nombre.toUpperCase());var split=a.split(" ");console.log(split),console.log(split.length),document.write(a.bold().fontcolor("red"));
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,13 +1,26 @@
-// Los Objetos Boleanos
-//var a = true;
-//var b = false;
-// El valor por defecto es false si y solo si es 0 -0 -1 undefined, string vacio.
-//var a = new Boolean(null);
-//console.log( a );
-
-var a = new Boolean();
-console.log( a.valueOf() );
-
-if( a.valueOf() ){
-    console.log("me imprimi!!");
-}
+// Los Objetos String
+// la variables tipo texto o cadena
+// En Js es un objeto
+var b = "Herrera";
+var a = new String("Fernando Herrera Pineda Chacon");
+console.log( a[0]);
+// MET. a Mayusculas
+console.log(a.toUpperCase());
+// MET. A minusculas
+console.log(a.toLowerCase());
+// MET. el primer index
+var i = a.indexOf("Herrera");
+console.log("la letra esta: ", i);
+// MET. El ultimo index
+var i = a.lastIndexOf("n");
+console.log("la letra esta: ", i);
+// MET. Desde donde quiero cortar el String
+var nombre = a.substr(0, a.indexOf(" "));
+console.log( nombre.toUpperCase() );
+// MET. Split Crea un Ojb con nuestra variable
+var split = a.split(" ");
+console.log( split );
+console.log( split.length );
+//=====
+// MET.
+document.write( a.bold().fontcolor("red"));
