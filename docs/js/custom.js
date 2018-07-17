e0f3de1cb901baa6e305bf364585f55ae0308b15
samsdial <diastudios@gmail.com>
2018-07-17 08:03:39 -0500

    Add Class learnig Javascript -- ExpresionesRegulares

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";function randomEntre(o,n){return Math.floor(Math.random()*(n-o+1)+o)}var PI=Math.PI,E=Math.E;console.log(PI),console.log(E);var num1=10.956789;console.log(num1),console.log(Math.round(num1)),console.log(Math.round(100*num1)/100),console.log(Math.floor(num1)),console.log(randomEntre(500,1e3)),console.log(Math.sqrt(10)),console.log(Math.pow(7,3));
+"use strict";var reg1=RegExp("A"),reg2=/a/,texto="la Respuesta de la suma es: 45 + 60 = 105";console.log(texto);var arr=texto.match(/\d{1,}|respuesta/gi);console.log(arr);
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,25 +1,36 @@
-// Objeto Math
-// Reside en el ObjetoGlobal
-var PI = Math.PI;
-var E = Math.E;
+// Expresiones Regulares
+// Mecanismo Excelente dentro de las cadenas regulares. Clases:
+// Explicita
+var reg1 = RegExp("A");
+// Literal
+var reg2 = /a/;
 
-console.log(PI);
-console.log(E);
-var num1 = 10.956789;
 
-console.log( num1 );
-// REdodndear numeros
-console.log( Math.round( num1 ));
-console.log( Math.round( num1*100 ) / 100 );// Dos decimales
+// ExREg de un arreglo /^n/ primer
+// ExREg de un arreglo /n$/ ultimo
+// ExREg de un arreglo /.../ los tres primeros
+// ExREg de un arreglo /[]/ buscar rango de numero o caracteres
+// ExREg de un arreglo /\s/ Primer Espacio
+// ExREg de un arreglo /[a-zA-Z0-9] = /\w/ Buscar en el rango del abecedario y numero
+// ExREg de un arreglo /[0-9] = /\d/ Buscar en el rango de numero (d=decimal)
+// ExREg de un arreglo /[aeiou]|[áéíóúñ]/ Buscar en el rango texto con tildes | = o expReg
+// ExREg de un arreglo por estructura de repetición /n+/g n representa la busqueda y el + sus repeteciones /g es el controlador todas las apariciones!
 
-console.log( Math.floor( num1 )); // quita todos los decimales sin redondeo
 
-//var rnd = Math.random();
-//console.log( rnd );
-function randomEntre( min, max ) {
-        return Math.floor( Math.random() * (max-min + 1) + min)
-}
-console.log( randomEntre( 500,1000 ));
+// ExREg de un arreglo por estructura de repetición /n?/g n representa la busqueda y el ? todos los caracteres del arreglo(ALERTA Si el string esta basio cuenta como expresion ) /g es el controlador todas las apariciones!
 
-console.log( Math.sqrt( 10 )); // Raiz cuadrada
-console.log( Math.pow( 7, 3 )); // Exponentes
+// ExREg de un string por estructura de repetición /n*/g n de 0 o mas veces es similar al anterio busca la expresion  y calza las demas letras
+// ExREg de un string por estructura de repetición /n{2,4}/g n representa la busqueda y el numero de veces
+
+// ExREg tienen tres controladores que nos permite controlar su comportamiento:
+
+// i = insensible a las Mayus o min
+// g = Todas las papairiciones
+// m = Multilinea
+// Lo importante no es saber si no saber quien sabe!!
+var texto = "la Respuesta de la suma es: 45 + 60 = 105";
+
+console.log(texto);
+var arr = texto.match( /\d{1,}|respuesta/ig ); // Return NUll
+
+console.log( arr );
