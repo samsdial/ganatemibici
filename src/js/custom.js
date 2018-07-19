3c71c7c671a66b3853a678d785cd760432e8a74f
samsdial <diastudios@gmail.com>
2018-07-19 08:50:25 -0500

    Add Class learnig Javascript -- Cookies

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,97 +1,46 @@
-// Manejo de errores en JavaScript
+// Cookies -- Escape, unescape
+//Es informacion que se guarda en la maquina del cliente:
+function  crearCookies( nombre, valor ) {
+    // Se denomina escape a el formato html de una Cookie para evitar errores al guardar
+    valor = escape(valor);
+    var hoy = new Date();
+    hoy.setMonth( hoy.getMonth() + 1 );
+    document.cookie = nombre + "=" + valor +";expieded="+ hoy.toUTCString() +";";
+}
+/*var demo = "123;123*123'123/ 123";
+console.log( demo );
+console.log( escape(demo) );
+// inverso del escape
+console.log( unescape( escape(demo) ) );*/
 
-//intenta
-/*
-try {
-    var a = 100;
-    if(a === 101){
-        throw 'Que mal'
-    }else{
-        throw 'oh oh!'
+// FN borrar cukie
+function borrarCookie( nombre ) {
+    var hoy = new Date();
+    hoy.setMonth( hoy.getMonth() - 1 );
+    document.cookie = nombre+"=x;expieres="+hoy.toUTCString() +";";
+
+}
+// Guardar cookies
+function getCookie( nombre ) {
+    var cookies = document.cookie;
+    var cookieArr = cookies.split("; ");
+    console.log( cookieArr );
+
+    for( var i = 0; i < cookieArr.length; i++){
+        var parArr = cookieArr[i].split("=");
+        cookieArr[i] = parArr;
+        if( parArr[0] === nombre){
+            return unescape( parArr[1] );
+        }
     }
-    console.log("el: ", a);
+    return undefined; //
 }
-// Si no intenta esto
-//Opcional
-catch (e){
-    if(e === "Que mal"){
-        console.log( "Error tipo 1" );
-    }
-    if(e == "oh oh!"){
-        console.log( "Error tipo 2" );
-    }
-}
-//
-finally {
-    console.log( "Finalmente" );
-}*/
-// 2manera
-/*
-try {
-    throw new Error('Error tipo 1');
-    console.log("el:");
-}
+console.log( getCookie("nombre") );
+crearCookies( "nombre", "Sams" );
+crearCookies( "apellido", "Martinez" );
+//borrarCookie( "Nombre" );
+//document.cookie = "Nombre=fern;ado"
+//var  cookies = document.cookie;
+//console.log( cookies );
 
-catch (e){
-    console.log(  e.message );
-}
-
-finally {
-    console.log( "Finalmente" );
-}*/
-/*
-try {
-    //Mandar un Objeto
-    throw {
-        nombreError: "error tipo 1",
-        acccio: "salir",
-        coderror: 1
-    };
-    console.log("el:");
-}
-
-catch (e){
-    console.log( e );
-    console.log( e.nombreError );
-    console.log( e.acccio );
-    console.log( e.coderror );
-    console.log(  "Parte del catch" );
-}
-
-finally {
-    console.log( "Finalmente" );
-}*/
-/*
-try {
-    // Mandando una Funcion
-    throw function () {
-        console.log( "Funcion del thorw" );
-    };
-    console.log("el:");
-}
-
-catch (e){
-    e();
-    console.log(  "parte del catch" );
-}
-
-finally {
-    console.log( "Finalmente" );
-}*/
-try {
-    //throw 1; //n Cualquier numero
-    // se recomienda usar el new Error por un error de Js
-    throw new Error('nombre de erro');
-}
-
-catch (e){
-    registroError( e );
-}
-
-finally {
-    console.log( "Finalmente" );
-}
-function registroError(e) {
-    var ahora = new Date();
-    console.log( "Se Registro un erro: ", e, "a las: ", ahora.getTime() );
-}
+// cookie siempre se puden ver
