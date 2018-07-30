65a6fbc3e2dbbb1152ab4cdef4314abfbce86eb2
samsdial <diastudios@gmail.com>
2018-07-30 16:40:59 -0500

    Add Class learnig Javascript -- funciones especiales call and apply

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,46 +1,29 @@
-// Cookies -- Escape, unescape
-//Es informacion que se guarda en la maquina del cliente:
-function  crearCookies( nombre, valor ) {
-    // Se denomina escape a el formato html de una Cookie para evitar errores al guardar
-    valor = escape(valor);
-    var hoy = new Date();
-    hoy.setMonth( hoy.getMonth() + 1 );
-    document.cookie = nombre + "=" + valor +";expieded="+ hoy.toUTCString() +";";
-}
-/*var demo = "123;123*123'123/ 123";
-console.log( demo );
-console.log( escape(demo) );
-// inverso del escape
-console.log( unescape( escape(demo) ) );*/
-
-// FN borrar cukie
-function borrarCookie( nombre ) {
-    var hoy = new Date();
-    hoy.setMonth( hoy.getMonth() - 1 );
-    document.cookie = nombre+"=x;expieres="+hoy.toUTCString() +";";
-
-}
-// Guardar cookies
-function getCookie( nombre ) {
-    var cookies = document.cookie;
-    var cookieArr = cookies.split("; ");
-    console.log( cookieArr );
-
-    for( var i = 0; i < cookieArr.length; i++){
-        var parArr = cookieArr[i].split("=");
-        cookieArr[i] = parArr;
-        if( parArr[0] === nombre){
-            return unescape( parArr[1] );
-        }
+// Funciones especiales: call, apply, bind
+var carro = {
+    color: "carro",
+    marca: "mazda",
+    imprimir: function () {
+        var salida = this.marca + " - " + this.color;
+        return salida;
     }
-    return undefined; //
-}
-console.log( getCookie("nombre") );
-crearCookies( "nombre", "Sams" );
-crearCookies( "apellido", "Martinez" );
-//borrarCookie( "Nombre" );
-//document.cookie = "Nombre=fern;ado"
-//var  cookies = document.cookie;
-//console.log( cookies );
+};
+var carro2 = {
+    color: "rojo",
+    marca: "toyoya"
+};
+console.log( carro.imprimir() );
+var logCarro = function ( arg1, arg2 ) {
+  console.log("Carro:  ", this.imprimir());
+  console.log("Argunentos:  ", arg1, arg2);
+  console.log("===================");
+};
+var logModeloCarro = logCarro.bind( carro );
 
-// cookie siempre se puden ver
+logModeloCarro();
+logModeloCarro("abc", "xyz");
+logModeloCarro.call( carro, "123", "456" );
+logModeloCarro.apply( carro, ["123", "456"] );
+
+// Sirven para:
+// funciones prestadas
+console.log( carro.imprimir.call( carro2 ) );
