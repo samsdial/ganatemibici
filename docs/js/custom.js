ec6b22c83f18d91d7ddca810fe3d1cab5095d478
samsdial <diastudios@gmail.com>
2018-07-31 14:56:17 -0500

    Add Class learnig Javascript -- Rotulando ciclos

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";var Persona=function(){this.nombre="Juan",this.apellido="Pineda",this.edad=30},juan=new Persona;Persona.prototype.direction="San José";for(var prop in juan)console.log(juan.hasOwnProperty(prop)),console.log(prop,": ",juan[prop]);for(var num in[1,2,3,4,5])console.log(num);[1,!0,3,!1,"nombre"].forEach(function(o){console.log(o)});
+"use strict";for(var i=1;i<=5;i++){console.log("i",i);for(var j=1;j<=5;j++){console.log("j",j);o:for(var x=1;x<=5;x++){console.log("x",x);break o}}}
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,33 +1,14 @@
-//For
-/*for (var i=0; i < 10; i++){
-    if( i == 5){
-        continue
+//Rotulando Ciclos:
+for_principal:
+for ( var i = 1; i <= 5 ; i++ ){
+    console.log("i", i);
+    for_secundario:
+    for( var j = 1; j<= 5; j++ ){
+        console.log("j", j);
+        for_terciario:
+        for( var x = 1; x <= 5; x++ ){
+            console.log("x", x);
+            break for_terciario;
+        }
     }
-    console.log(i);
-}*/
-// For in
-var Persona = function(){
-  this.nombre = "Juan";
-  this.apellido = "Pineda";
-  this.edad = 30;
-};
-var juan = new Persona();
-Persona.prototype.direction = "San José";
-
-// Reflejo se evalua en si mismo para saber si es una caracteristica del objeto.
-for( var prop in juan ){
-    console.log( juan.hasOwnProperty( prop ));
-    console.log( prop,": ", juan[prop]);
 }
-for( var num in [1,2,3,4,5]){
-    console.log(num)
-}
-// For eache de los arreglos
-[1,true,3,false,"nombre"].forEach(function ( val ) {
-   console.log( val );
-});
-/*
-
-for( prop in juan ){
-    console.log( prop, juan[prop]);
-}*/
