a6557b742ff23aec63aa66fc199f57f064c8bde7
samsdial <diastudios@gmail.com>
2018-07-31 10:12:35 -0500

    Add Class learnig Javascript -- switch estructure

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";function getNombre(e,o){var l=e||null||"sin apellido";console.log(l)}getNombre();
+"use strict";var mes=40;switch(mes){case 40:console.log("Enero");break;case 2:console.log("Febrero");break;case 3:console.log("Marzo");break;default:console.log("cualquier cosa")}
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,48 +1,15 @@
-// If ... Else
-/*
-var nota = 55;
-if( nota >= 90 ){
-   console.log("A")
-}else if( nota >= 80){
-    console.log("B")
-}else if( nota >= 70){
-    console.log("C")
-}else if( nota >= 60){
-    console.log("D")
-}else{
-    if ( nota > 50){
-        console.log("F");
-    }
-    console.log("Algo paso por aqui..")
+//Switch
+var mes = 40;
+switch (mes){
+    case ( 10 > 5 )? 40:1 :
+        console.log("Enero");
+        break;
+    case 2:// se puede agregar funciones
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    default:
+        console.log("cualquier cosa")
 }
-console.log("termino el codigo");*/
-/*var a = undefined;
-var b = 20;*/
-// if de Validación Corta
-//var c = ( a > b)? a : b;
-
-
-// Avanzado
-/*var c = ( a > b)? function () {
-    console.log("A es mayor a B");
-    return a;
-}() : function () {
-    console.log("B es mayor a A");
-    return b;
-}();
-console.log(c);*/
-/*if( a > b){
-    c = a;
-}else{
-    c = b;
-}
-console.log(c);*/
-//
-// var c = a || b;
-// console.log(c);
-//Funciones que esperan parametros
-function getNombre( nombre, apellido ) {
-    var nomb = nombre || null || "sin apellido";
-    console.log( nomb );
-}
-getNombre();
