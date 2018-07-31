b8b5e80f71f1506e6b1834a4e8c89202599d5a9e
samsdial <diastudios@gmail.com>
2018-07-31 16:31:55 -0500

    Add Class learnig Javascript -- Eventos del Navegador

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";var segundos=0,juan={nombre:"juan",edad:30,imprimir:function(){var n=this;setTimeout(function(){console.log(n),console.log(n.nombre,n.edad)},1e3)}};juan.imprimir();
+"use strict";document.onmousedown=function(o){2!==o.button?console.log("no hay problema"):alert("click bloqueado")},document.onmouseup=function(){var o=window.getSelection().toString();console.log(o)};
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,37 +1,25 @@
-//funciones de tiempo:
-// cuando queremos que se ejecute cuando pase x cantidad de segundos.
+// evntos 101
+//click
+//focus
+//tipo de evento
 /*
+function evento( arg ) {
+ console.log("me dispare");
+ console.log( arg.x, arg.y );
+}
+var objeto = document.getElementById('demoObj');
 
-setTimeout( function () {
-    console.log("paso un segundo")
-}, 1000);
-*/
-
-// cuando queremos que se ejecute numero de veces.
-var segundos = 0;
-/* infinito
-setInterval( function () {
-    segundos ++;
-    console.log("seg", segundos);
-}, 1000);*/
-/*var intervalo = setInterval( function () {
-    segundos ++;
-    console.log("seg", segundos);
-    if( segundos === 5){
-        clearInterval( intervalo );
+objeto.addEventListener("click", evento);*/
+// Bloqueado el click derecho:
+document.onmousedown = function (arg) {
+    if(arg.button === 2 ){
+        alert("click bloqueado");
+        return;
     }
-}, 1000);*/
-var juan = {
-    nombre: "juan",
-    edad: 30,
-    imprimir: function () {
-        var self = this;
-        setTimeout( function () {
-            console.log(self);
-            console.log(self.nombre, self.edad);
-        },1000);
-
-    }
+    console.log("no hay problema")
 };
+document.onmouseup = function () {
+    var texto = window.getSelection().toString();
+    console.log(texto);
 
-juan.imprimir();
+}
