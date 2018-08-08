2e48288e5160384ef13d7b537432b8858349fa19
samsdial <diastudios@gmail.com>
2018-08-08 09:09:37 -0500

    Add Class learnig Javascript -- Eventos del Navegador Seleccionar de la pag

--- src/js/custom.js
+++ src/js/custom.js
@@ -12,12 +12,15 @@ var objeto = document.getElementById('demoObj');
 objeto.addEventListener("click", evento);*/
 // Bloqueado el click derecho:
 document.onmousedown = function (arg) {
+    // click en Boton Derecho
     if(arg.button === 2 ){
         alert("click bloqueado");
         return;
     }
+    // click en Boton izq
     console.log("no hay problema")
 };
+// Seleccionar lo que esta el la pagina web
 document.onmouseup = function () {
     var texto = window.getSelection().toString();
     console.log(texto);
